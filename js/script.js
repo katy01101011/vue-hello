88faf46d3ab93d38db5216e64de4af6c1bf85fa4
katy01101011 <katy.91560@gmail.com>
2022-05-06 09:34:01 +0200

    add: title + img

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const app = new Vue (
+    {
+        el: '#root',
+        data: {
+            title: 'Hello World!',
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW0qWHDU30Y_IOCr_6GBSzq0wCQCCVoYwsiA&usqp=CAU',
+        }
+    }
+)
